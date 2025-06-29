7a4beb0fda0d936adc30c2e16ad0fe28dc4d86b4
joshpled <joshuapleduc@gmail.com>
2025-06-29 11:52:43 -0400

    converted nomatch comp;onent to error boundary

--- src/views/NoMatch.jsx
+++ src/views/NoMatch.jsx
@@ -1,14 +1,30 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router';
+import {
+    isRouteErrorResponse,
+    useRouteError,
+} from "react-router";
 
-export default function NoMatch() {
-    const navigate = useNavigate();
+export default function ErrorBoundary() {
+    const error = useRouteError();
 
-    useEffect(() => {
-        navigate('/dashboard');
-    }, [navigate]);
-
-    return (
-        <div>Redirecting...</div>
-    );
+    if (isRouteErrorResponse(error)) {
+        return (
+            <div>
+                <h1>
+                    {error.status} {error.statusText}
+                </h1>
+                <p>{error.data}</p>
+            </div>
+        );
+    } else if (error instanceof Error) {
+        return (
+            <div>
+                <h1>Error</h1>
+                <p>{error.message}</p>
+                <p>The stack trace is:</p>
+                <pre>{error.stack}</pre>
+            </div>
+        );
+    } else {
+        return <h1>Unknown Error</h1>;
+    }
 }
